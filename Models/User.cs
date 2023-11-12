60ab725e9ca2c273c335bcc5115d1158966b28df
Trâu <Tru@LAPTOP-4BFMLM51>
2023-11-12 16:31:06 +0700

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Petshop.Models
+{
+    public class User
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+
+        public string Password { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Address { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
